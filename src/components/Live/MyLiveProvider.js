a3539a0ae0a65fee4c552fa591a8dbd4e0aebc6e
Brian Howard <bhoward@depauw.edu>
2020-01-11 10:49:30 -0500

    Removed noInline

--- src/components/Live/MyLiveProvider.js
+++ src/components/Live/MyLiveProvider.js
@@ -1,43 +1,41 @@
 import React, { Component } from 'react';
 
 import MyLiveContext from './MyLiveContext';
-import { generateElement, renderElementAsync } from '../../utils/transpile';
+import { generateElement } from '../../utils/transpile';
 
 export default class MyLiveProvider extends Component {
   // eslint-disable-next-line camelcase
   UNSAFE_componentWillMount() {
-    const { code, language, scope, noInline } = this.props;
+    const { code, language, scope } = this.props;
 
-    this.transpile({ code, language, scope, noInline });
+    this.transpile({ code, language, scope });
   }
 
   componentDidUpdate({
     code: prevCode,
     language: prevLanguage,
-    scope: prevScope,
-    noInline: prevNoInline
+    scope: prevScope
   }) {
-    const { code, language, scope, noInline } = this.props;
+    const { code, language, scope } = this.props;
     if (
       code !== prevCode ||
       language !== prevLanguage ||
-      scope !== prevScope ||
-      noInline !== prevNoInline
+      scope !== prevScope
     ) {
-      this.transpile({ code, language, scope, noInline });
+      this.transpile({ code, language, scope });
     }
   }
 
   onChange(code) {
-    const { language, scope, noInline } = this.props;
-    this.transpile({ code, language, scope, noInline });
+    const { language, scope } = this.props;
+    this.transpile({ code, language, scope });
   }
 
   onError(error) {
     this.setState({ error: error.toString() });
   }
 
-  transpile({ code, language, scope, noInline = false }) {
+  transpile({ code, language, scope }) {
     // Transpilation arguments
     const input = {
       code,
@@ -53,12 +51,7 @@ export default class MyLiveProvider extends Component {
     const state = { unsafeWrapperError: undefined, error: undefined };
 
     try {
-      if (noInline) {
-        this.setState({ ...state, element: null }); // Reset output for async (no inline) evaluation
-        renderElementAsync(input, renderElement, errorCallback);
-      } else {
-        renderElement(generateElement(input, errorCallback));
-      }
+      renderElement(generateElement(input, errorCallback));
     } catch (error) {
       this.setState({ ...state, error: error.toString() });
     }
@@ -79,7 +72,6 @@ export default class MyLiveProvider extends Component {
           onChange: this.onChange.bind(this)
         }}
       >
-        <strong>Foo!</strong>
         {children}
       </MyLiveContext.Provider>
     );
@@ -88,7 +80,6 @@ export default class MyLiveProvider extends Component {
 
 MyLiveProvider.defaultProps = {
   code: '',
-  noInline: false,
   language: 'js',
   disabled: false
 };
--- src/utils/transpile/index.js
+++ src/utils/transpile/index.js
@@ -7,25 +7,3 @@ export const generateElement = ({ code = '', language = 'js', scope = {} }, erro
   return errorBoundary(evalCode(transformed, scope), errorCallback);
 };
 
-export const renderElementAsync = (
-  { code = '', language = 'js', scope = {} },
-  resultCallback,
-  errorCallback
-  // eslint-disable-next-line consistent-return
-) => {
-  const render = element => {
-    if (typeof element === 'undefined') {
-      errorCallback(new SyntaxError('`render` must be called with valid JSX.'));
-    } else {
-      resultCallback(errorBoundary(element, errorCallback));
-    }
-  };
-
-  if (!/render\s*\(/.test(code)) {
-    return errorCallback(
-      new SyntaxError('No-Inline evaluations must call `render`.')
-    );
-  }
-
-  evalCode(transform(code, language).jscode, { ...scope, render });
-};
