eb8855806acf338a40d95958bae630a2e0a442d1
Brian Howard <bhoward@depauw.edu>
2020-01-17 23:28:58 -0500

    Revised ocaml evaluator with extra libraries

--- src/utils/transpile/evalCode.js
+++ src/utils/transpile/evalCode.js
@@ -1,16 +1,24 @@
 const evalCode = (code, scope) => {
   // ignores scope...
+  const _consoleLog = console.log;
   const _consoleError = console.error;
+  let output = '';
   let errs = '';
+  console.log = (...args) => {
+    return args.forEach(argument => {
+      output += argument + '\n'
+    });
+  };
   console.error = (...args) => {
     return args.forEach(argument => {
       errs += argument + '\n'
     });
-  }
-  let res = evaluator.execute(code)
+  };
+  let res = evaluator.execute(code);
+  console.log = _consoleLog;
   console.error = _consoleError;
 
-  return res + '\n' + errs; // TODO fix this
+  return res + '\n' + output + '\n' + errs; // TODO fix this
 };
 
 export default evalCode;
