4284d2b8eae0907bc471c613528c06e0fce2da74
Brian Howard <bhoward@depauw.edu>
2020-03-01 00:02:54 -0500

    Switched to sketch.sh engine

--- src/utils/transpile/index.js
+++ src/utils/transpile/index.js
@@ -1,10 +1,8 @@
-import transform from './transform';
 import errorBoundary from './errorBoundary';
 import evalCode from './evalCode';
 
 export const generateElement =
   ({ code = '', language = 'ocaml', hidden = false }, successCallback, errorCallback) => {
-    const {mlcode, errors} = transform(code, language);
-    successCallback(errorBoundary(evalCode(mlcode, errors, hidden), errorCallback));
+    successCallback(errorBoundary(evalCode(code, language, hidden), errorCallback));
   };
 
