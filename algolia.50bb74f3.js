03603a5c051a23690403b362d73be4a8a38b32b3
Brian Howard <bhoward@depauw.edu>
2020-04-22 00:09:58 -0400

    Deploy website version based on 1e104ba26cb28104b00fca8c22a7bce693cbb3bf

--- algolia.50bb74f3.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[78],{346:function(n,w,o){}}]);
---
+++ algolia.55a9646a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[79],{347:function(n,w,o){}}]);
