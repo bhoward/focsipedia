993f82c75aaed09096c953a9d753219cc282b131
Brian Howard <bhoward@depauw.edu>
2020-04-28 21:37:07 -0400

    Deploy website version based on 6ed35821966cd4114fcfcc7474dc94395797d195

---
+++ ebd68d89.5ece2371.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[125],{228:function(e){e.exports=JSON.parse('{"permalink":"/focsipedia/blog/page/2","page":2,"postsPerPage":10,"totalPages":4,"totalCount":34,"previousPage":"/focsipedia/blog","nextPage":"/focsipedia/blog/page/3"}')}}]);
